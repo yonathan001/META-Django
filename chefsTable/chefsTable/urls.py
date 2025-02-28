f9e743793b48d1d3952ca6a0fabbd1478df26248
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-28 22:34:33 +0300

    myapp2

--- chefsTable/chefsTable/urls.py
+++ chefsTable/chefsTable/urls.py
@@ -22,4 +22,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home two'),
+    path('',views.home, name='home one'),
+    path('',views.home, name='home two'),
 ]
