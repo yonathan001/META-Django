c6d4fbd8fb89f3709a03e9578ce634d7fa980974
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-03-04 14:21:19 +0300

    re_build

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for chefsTable project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
 ]
