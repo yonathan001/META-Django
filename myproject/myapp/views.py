9b326cb0969481f5b6e5686565010bba63c3851f
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-03-06 12:10:05 +0300

    about

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def homepage(request):
    return HttpResponse("This is the homepage of the website")
+
+def about(request):
+   return HttpResponse("This is the about page of the website")
