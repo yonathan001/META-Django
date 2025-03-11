9bcd7dd53d8af4d0fd89491b597dd3c308319cab
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-03-11 15:26:00 +0300

    migartion

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.model):
+class Menu(models.Model):
     Name = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=100)
     price = models.IntegerField()
