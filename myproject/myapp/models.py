9d3bb5120d03c8762d036e5e6f8d3edd64c90072
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-04-02 18:56:44 +0300

    new_proj

--- myproject/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Menu(models.Model):
-    Name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
-
-    
--- myproject2/myproject2/urls.py
+++ myproject2/myproject2/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path
 from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-   
+    path('', views.home, name='home'),  # Root URL
 ]
